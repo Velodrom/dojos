68a199d920c1be5ce2c60060d78716f424093658
Janne Aho <janne.aho@nokia.com>
2016-12-21 09:42:46 +0200

    Enable only first failing unit test for python filter test

--- py/filter.py
+++ py/filter.py
@@ -2,35 +2,42 @@ import unittest
 
 epsilon = 0.001
 
-# Implement this filter function
-def fir_filter(inputs):
-	coefs = [0.5, 0.3, 0.2, 0.1]
-	return None
+def FirFilter(inputs):
+	coefs     = [0.5, 0.3, 0.2, 0.1]
+	output    = [0]
+	return output
 
 class TestFIRFilter(unittest.TestCase):
 
-	def test_zeroInput(self):
+	def test_ZeroInput(self):
 		inputs    = [0]
 		refOutput = [0, 0, 0, 0]
-		output    = fir_filter(inputs)
+		output    = FirFilter(inputs)
 
 		self.assertEqual(refOutput, output)
 
-	def test_impulseInput(self):
-		inputs    = [1.0]
-		refOutput = [0.5, 0.3, 0.2, 0.1]
-		output    = fir_filter(inputs)
+#	def test_ImpulseInput(self):
+#		inputs    = [1.0]
+#		refOutput = [0.5, 0.3, 0.2, 0.1]
+#		output    = FirFilter(inputs)
+#
+#		self.assertEqual(refOutput, output)
+#
+#	def test_OutputLengthWithShortInput(self):
+#		inputs    = [1.0, 1.0]
+#		refOutput = [0.5, 0.8, 0.5, 0.3, 0.1]
+#		output    = FirFilter(inputs)
+#
+#		self.AssertListAlmostEqual(refOutput, output, epsilon)
+#
+#	def test_OutputLengthWithLongInput(self):
+#		inputs    = [1.0, 1.0, 1.0, 1.0, 1.0]
+#		refOutput = [0.5, 0.8, 1.0, 1.1, 1.1, 0.6, 0.3, 0.1]
+#		output    = FirFilter(inputs)
+#
+#		self.AssertListAlmostEqual(refOutput, output, epsilon)
 
-		self.assertEqual(refOutput, output)
-
-	def test_outputLength(self):
-		inputs    = [1.0, 1.0]
-		refOutput = [0.5, 0.8, 0.5, 0.3, 0.1]
-		output    = fir_filter(inputs)
-
-		self.assertListAlmostEqual(refOutput, output, epsilon)
-
-	def assertListAlmostEqual(self, list1, list2, tol):
+	def AssertListAlmostEqual(self, list1, list2, tol):
 		self.assertEqual(len(list1), len(list2))
 		for a, b in zip(list1, list2):
 			self.assertTrue(abs(a-b) < tol)
--- py/sln/filter_example.py
+++ py/sln/filter_example.py
@@ -2,7 +2,7 @@ import unittest
 
 epsilon = 0.001
 
-def fir_filter(inputs):
+def FirFilter(inputs):
 	coefs     = [0.5, 0.3, 0.2, 0.1]
 	filterLen = len(coefs)
 	inputLen  = len(inputs)
@@ -26,28 +26,35 @@ def fir_filter(inputs):
 
 class TestFIRFilter(unittest.TestCase):
 
-	def test_zeroInput(self):
+	def test_ZeroInput(self):
 		inputs    = [0]
 		refOutput = [0, 0, 0, 0]
-		output    = fir_filter(inputs)
+		output    = FirFilter(inputs)
 
 		self.assertEqual(refOutput, output)
 
-	def test_impulseInput(self):
+	def test_ImpulseInput(self):
 		inputs    = [1.0]
 		refOutput = [0.5, 0.3, 0.2, 0.1]
-		output    = fir_filter(inputs)
+		output    = FirFilter(inputs)
 
 		self.assertEqual(refOutput, output)
 
-	def test_outputLength(self):
+	def test_OutputLengthWithShortInput(self):
 		inputs    = [1.0, 1.0]
 		refOutput = [0.5, 0.8, 0.5, 0.3, 0.1]
-		output    = fir_filter(inputs)
+		output    = FirFilter(inputs)
 
-		self.assertListAlmostEqual(refOutput, output, epsilon)
+		self.AssertListAlmostEqual(refOutput, output, epsilon)
 
-	def assertListAlmostEqual(self, list1, list2, tol):
+	def test_OutputLengthWithLongInput(self):
+		inputs    = [1.0, 1.0, 1.0, 1.0, 1.0]
+		refOutput = [0.5, 0.8, 1.0, 1.1, 1.1, 0.6, 0.3, 0.1]
+		output    = FirFilter(inputs)
+
+		self.AssertListAlmostEqual(refOutput, output, epsilon)
+
+	def AssertListAlmostEqual(self, list1, list2, tol):
 		self.assertEqual(len(list1), len(list2))
 		for a, b in zip(list1, list2):
 			self.assertTrue(abs(a-b) < tol)
