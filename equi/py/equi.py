060889ac2e3a48a65b06a404eb9d7cd23d43f0cd
ShoeTea <sutirtho.majumdar@gmail.com>
2020-01-16 11:32:33 +0200

    This solution finds the first equilibrium index

--- equi/py/equi.py
+++ equi/py/equi.py
@@ -1,16 +1,23 @@
 import unittest
-
 def find_equi_index(inArray, numElements):
-	# Find the equilibrium index of input array
-	return -1
+    upper_index_sum= 0
+    for x in range(numElements):
+        upper_index_sum = upper_index_sum + inArray[x]
+    lower_index_sum =0
+    for i in range(numElements):
+        lower_index_sum = lower_index_sum + inArray[i]
+        if lower_index_sum == upper_index_sum:
+            return i
+        upper_index_sum = upper_index_sum - inArray[i]
+    return -1
 
 class TestEquiIndex(unittest.TestCase):
-	def test_signelNumber(self):
-		inArray = [3]
-		expectedP = 0
-		P = find_equi_index(inArray, len(inArray))
-		self.assertEqual(expectedP, P)
+    def test_signelNumber(self):
+        inArray = [2,-3,4,-2,1]
+        expectedP = 0
+        P = find_equi_index(inArray, len(inArray))
+        self.assertEqual(expectedP, P)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
