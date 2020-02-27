75649368f62c2a6810a122125cd5ae9dacebf971
mzxstosch <mzxstosch@gmail.com>
2020-02-27 19:02:44 +0200

    Added missing alphabet solution

--- missing_alphabet/py/first_missing_alphabet.py
+++ missing_alphabet/py/first_missing_alphabet.py
@@ -2,7 +2,28 @@ import unittest
 
 def first_missing_alphabet(inputString):
 	# Find the first missing alphabet character
-	return -1
+	fullalphabetstring = "abcdefghijklmnopqrstuvwxyz"
+
+	alphabetregister = {}
+
+	#add letters to dictionary
+	for letter in fullalphabetstring:
+		alphabetregister[letter] = 0
+
+	lowercaseinputstring = inputString.lower()
+
+	for letter in lowercaseinputstring:
+		#check if letter is in alphabetregister as key => abcd... but not @!$...:
+		if letter in alphabetregister:
+			alphabetregister[letter] = 1
+
+	##check if/which letter is missing
+	for k,v in alphabetregister.items():
+		if v == 0:
+			return k
+
+
+	return ""
 
 class Fma_tests(unittest.TestCase):
 	def test_single_character(self):
@@ -11,6 +32,41 @@ class Fma_tests(unittest.TestCase):
 		foundMissing = first_missing_alphabet(inString)
 		self.assertEqual(expectedMissing, foundMissing)
 
+	def emptystring(self):
+		inString = ""
+		expectedMissing = "a"
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
+
+	def nonalphabeticcharacters(self):
+		inString = "@!"
+		expectedMissing = "a"
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
+
+	def startingwithlastcharactersofalphabet(self):
+		inString = "za"
+		expectedMissing = "b"
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
+
+	def recurringcharacters(self):
+		inString = "aaaaa"
+		expectedMissing = "b"
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
+
+	def providedcomplexstring(self):
+		inString = "@aYBed56$=CAg"
+		expectedMissing = "f"
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
+
+	def completealphabet(self):
+		inString = "abcdefghijklmnopqrstuvwxyz"
+		expectedMissing = ""
+		foundMissing = first_missing_alphabet(inString)
+		self.assertEqual(expectedMissing, foundMissing)
 
 if __name__ == '__main__':
 	unittest.main()
