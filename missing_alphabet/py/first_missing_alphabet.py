e318ac20279995d759986649ba925964e3ccc035
Narahari Kasagatta Ramesh <naraharikasagattaramesh@gmail.com>
2020-03-17 21:28:42 +0200

    Ver.1 Solution for first missing alphabet
    Pseudo Algorithm:
    
    Create an empty string 
      Check whether each character is an alphabet
         If Yes add to the created empty string
    
    Convert the whole string to lower case
    Remove all the duplicate characters comparing to dictionary
    
    Create an empty list
    Compare each character with lowercase ASCII alphabet
    Append all the missing characters to the list 
    return the first index of the list

--- missing_alphabet/py/first_missing_alphabet.py
+++ missing_alphabet/py/first_missing_alphabet.py
@@ -1,16 +1,40 @@
 import unittest
+import string 
 
 def first_missing_alphabet(inputString):
-	# Find the first missing alphabet character
-	return -1
+  # Find the first missing alphabet character
+  
+  #Extract alphabet from string
+  alphaString = ""
+  for char in inputString:
+    if char.isalpha():
+      alphaString += char
+  
+    # Convert string to lower case and sorted
+  alphaSorted = sorted(alphaString.lower())
+    
+    #Remove duplicate value 
+  alphaUnique = list(dict.fromkeys(alphaSorted))
+    
+    # Create empty list for storing all missing
+    # characters in alphabetical order
+  letter = []
+  for letters in string.ascii_lowercase:
+    if letters not in alphaUnique:
+      letter.append(letters)
+
+  print ("List of missing characters in", inputString)
+  print (letter)
+  return (letter[0])
 
 class Fma_tests(unittest.TestCase):
-	def test_single_character(self):
-		inString = "b"
-		expectedMissing = "a"
-		foundMissing = first_missing_alphabet(inString)
-		self.assertEqual(expectedMissing, foundMissing)
-
-
+  def test_single_character(self):
+    inString = "@aYBed56$=CAg"
+    expectedMissing = "f"
+    foundMissing = first_missing_alphabet(inString)
+    self.assertEqual(expectedMissing, foundMissing)
+    if expectedMissing == foundMissing:
+      print ("The first missing alphabet is", foundMissing)
+  
 if __name__ == '__main__':
-	unittest.main()
+  unittest.main()
